ae2f396cbcef522814a1b824cffd062003bec1fa
weler s martins <welerson194@gmail.com>
2026-02-20 10:21:37 -0300

    p

--- assets/js/components.js
+++ assets/js/components.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     
-    // --- 1. IMPLEMENTAÇÃO DO HEADER DO GERAL DO SITE---
+    // --- 2. IMPLEMENTAÇÃO DO HEADER DO GERAL DO SITE---
     const headerElementeGeneral = document.querySelector("#header-general-placeholder");
     if (headerElementeGeneral) {
         headerElementeGeneral.innerHTML = `
@@ -72,9 +72,6 @@ document.addEventListener("DOMContentLoaded", function () {
         </nav>
         `;
     }
-
-
-
 
 
     // --- 3. SEÇÃO DE CONTATO REUTILIZÁVEL ---
@@ -183,76 +180,15 @@ document.addEventListener("DOMContentLoaded", function () {
     `;
     }
 
-
-
-
-    // --- 2. IMPLEMENTAÇÃO DO FOOTER GERAL DO SITE ---
-    const footerElementGeneral = document.querySelector("#footer-general-placeholder");
-    if (footerElementGeneral) {
-        footerElementGeneral.innerHTML = `
-        <footer class="bg-black text-light pt-5 pb-3">
-            <div class="container-xl">
-                <div class="row gy-4 d-flex justify-content-center">
-                    <div class="col-lg-4 text-center text-lg-start">
-                        <a class="d-flex align-items-center justify-content-center justify-content-lg-start text-decoration-none mb-3" href="/focus">
-                            <img src="../assets/images/logo.png" alt="Logo" style="height: 40px;" class="me-2">
-                            <h4 class="logo-text m-0 text-white fw-bold" style="letter-spacing: 1px;">FOCUS SERVICE</h4>
-                        </a>
-                        <p class="small pe-lg-4">Soluções inteligentes em serviços e gestão para condomínios e empresas. Excelência, confiança e resultados tangíveis.</p>
-                        <p class="small mt-2 text-danger">CNPJ: 00.000.000/0000-00</p>
-                    </div>
-
-                    <div class="col-6 col-lg-2 offset-lg-1" style="width: 150px;">
-                        <h6 class="fw-bold mb-3 text-white">Navegação</h6>
-                        <ul class="list-unstyled footer-links">
-                            <li><a href="./index.html" class="text-white text-decoration-none">Início</a></li>
-                            <li><a href="./focus/empresa.html" class="text-white text-decoration-none">Empresa</a></li>
-                            <li><a href="./servicos/todos-servicos.html" class="text-white text-decoration-none">Serviços</a></li>
-                            <li><a href="./focus/contato.html" class="text-white text-decoration-none">Contato</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-6 col-lg-2" style="width: 150px;">
-                        <h6 class="fw-bold mb-3">Suporte</h6>
-                        <ul class="list-unstyled footer-links">
-                            <li><a href="./focus/duvidas.html" class="text-light text-decoration-none">Dúvidas</a></li>
-                            <li><a href="./focus/privacidade.html" class="text-light text-decoration-none">Privacidade</a></li>
-                            <li><a href="./focus/termos.html" class="text-light text-decoration-none">Termos</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-lg-3 text-center text-lg-start">
-                        <h6 class="fw-bold mb-3 text-white">Redes Sociais</h6>
-                        <div class="d-flex gap-3 justify-content-center justify-content-lg-start mt-3">
-                            <a href="https://www.instagram.com/invites/contact/?i=1gwwmmm8k9l1z&utm_content=pqi2lcg" target="_blank" rel="noopener noreferrer">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-instagram" viewBox="0 0 16 16">
-                                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
-                                </svg>
-                            </a>
-                            <a href="https://www.facebook.com/focusrsservice?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-facebook" viewBox="0 0 16 16">
-                                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <hr class="my-4 opacity-10">
-                <div class="text-center">
-                    <p class="mb-0 text-white opacity-50" style="font-size: 0.85rem;">&copy; 2022 Focus Service | Portaria e Limpeza</p>
-                </div>
-            </div>
-        </footer>
-        `;
-    }
-    // --- 2. IMPLEMENTAÇÃO DO FOOTER ---
+    // --- 4. IMPLEMENTAÇÃO DO FOOTER ---
     const footerElement = document.querySelector("#footer-placeholder");
     if (footerElement) {
         footerElement.innerHTML = `
         <footer class="bg-black text-light pt-5 pb-3">
             <div class="container-xl">
+
                 <div class="row gy-4 d-flex justify-content-center">
+
                     <div class="col-lg-4 text-center text-lg-start">
                         <a class="d-flex align-items-center justify-content-center justify-content-lg-start text-decoration-none mb-3" href="/focus/">
                             <img src="./assets/images/logo.png" alt="Logo" style="height: 40px;" class="me-2">
@@ -265,7 +201,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     <div class="col-6 col-lg-2 offset-lg-1" style="width: 150px;">
                         <h6 class="fw-bold mb-3 text-white">Navegação</h6>
                         <ul class="list-unstyled footer-links">
-                            <li><a href="./index.html" class="text-white text-decoration-none">Início</a></li>
+                            <li><a href="./focus" class="text-white text-decoration-none">Início</a></li>
                             <li><a href="./focus/empresa.html" class="text-white text-decoration-none">Empresa</a></li>
                             <li><a href="./servicos/todos-servicos.html" class="text-white text-decoration-none">Serviços</a></li>
                             <li><a href="./focus/contato.html" class="text-white text-decoration-none">Contato</a></li>
@@ -306,4 +242,76 @@ document.addEventListener("DOMContentLoaded", function () {
         </footer>
         `;
     }
+
+
+
+    // --- 4. IMPLEMENTAÇÃO DO FOOTER GERAL DO SITE ---
+    const footerElementGeneral = document.querySelector("#footer-general-placeholder");
+    if (footerElementGeneral) {
+        footerElementGeneral.innerHTML = `
+        <footer class="bg-black text-light pt-5 pb-3">
+            <div class="container-xl">
+
+                <div class="row gy-4 d-flex justify-content-center">
+
+                    <div class="col-lg-4 text-center text-lg-start">
+                        <a class="d-flex align-items-center justify-content-center justify-content-lg-start text-decoration-none mb-3" href="/focus">
+                            <img src="../assets/images/logo.png" alt="Logo" style="height: 40px;" class="me-2">
+                            <h4 class="logo-text m-0 text-white fw-bold" style="letter-spacing: 1px;">FOCUS SERVICE</h4>
+                        </a>
+                        <p class="small pe-lg-4">Soluções inteligentes em serviços e gestão para condomínios e empresas. Excelência, confiança e resultados tangíveis.</p>
+                        <p class="small mt-2 text-danger">CNPJ: 00.000.000/0000-00</p>
+
+                    </div>
+
+                    <div class="col-6 col-lg-2 offset-lg-1" style="width: 150px;">
+                        <h6 class="fw-bold mb-3 text-white">Navegação</h6>
+                        <ul class="list-unstyled footer-links">
+                            <li><a href="./focus" class="text-white text-decoration-none">Início</a></li>
+                            <li><a href="./empresa.html" class="text-white text-decoration-none">Empresa</a></li>
+                            <li><a href="./servicos/todos-servicos.html" class="text-white text-decoration-none">Serviços</a></li>
+                            <li><a href="./focus/contato.html" class="text-white text-decoration-none">Contato</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6 col-lg-2" style="width: 150px;">
+                        <h6 class="fw-bold mb-3">Suporte</h6>
+                        <ul class="list-unstyled footer-links">
+                            <li><a href="./focus/duvidas.html" class="text-light text-decoration-none">Dúvidas</a></li>
+                            <li><a href="./focus/privacidade.html" class="text-light text-decoration-none">Privacidade</a></li>
+                            <li><a href="./focus/termos.html" class="text-light text-decoration-none">Termos</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 text-center text-lg-start">
+                        <h6 class="fw-bold mb-3 text-white">Redes Sociais</h6>
+                        <div class="d-flex gap-3 justify-content-center justify-content-lg-start mt-3">
+                            <a href="https://www.instagram.com/invites/contact/?i=1gwwmmm8k9l1z&utm_content=pqi2lcg" target="_blank" rel="noopener noreferrer">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-instagram" viewBox="0 0 16 16">
+                                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+                                </svg>
+                            </a>
+                            <a href="https://www.facebook.com/focusrsservice?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-facebook" viewBox="0 0 16 16">
+                                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <hr class="my-4 opacity-10">
+                <div class="text-center">
+                    <p class="mb-0 text-white opacity-50" style="font-size: 0.85rem;">&copy; 2022 Focus Service | Portaria e Limpeza</p>
+                </div>
+            </div>
+        </footer>
+        `;
+    }
+
+
+
+
+
+
 });
